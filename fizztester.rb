0419440efcd647a06dd7a4f53fb1db12ef6d0249
hugosykes <hugo@denmeadfarm.co.uk>
2017-09-19 16:00:25 +0100

    Initialized rspec, updated the tester

--- fizztester.rb
+++ fizztester.rb
@@ -1,9 +1,21 @@
 require './fizzbuzz'
 describe 'Fizzbuzz' do
-  it 'checks 15' do
-    expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+  [1,2,4,7,8,11,13,14,16,17,19,22,23,26].each do |a|
+    it "checks #{a}" do
+      expect(fizzbuzz(a)).to eq a
+    end
   end
-  it 'checks 18' do
-    expect(fizzbuzz(18)).to eq 'Fizz'
+  [3,6,9,12,18,21,24,27].each do |a|
+    it "checks #{a}" do
+      expect(fizzbuzz(a)).to eq "Fizz"
+    end
+  end
+  [5,10,20,25].each do |a|
+    it "checks #{a}" do
+      expect(fizzbuzz(a)).to eq "Buzz"
+    end
+  end
+  it "checks 15" do
+    expect(fizzbuzz(15)).to eq "Fizzbuzz"
   end
 end
